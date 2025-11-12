92612375e884da333e0fcb8dbaf33a8142fa266d
Franz Holzinger <franz.holzinger@festwein.de>
2025-11-12 18:31:24 +0100

    TYPO3 13

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,14 +3,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'RDCT Frontend redirects',
     'description' => 'Adds redirects based on "&RDCT" parameter in Frontend',
     'category' => 'fe',
-    'state' => 'alpha',
+    'state' => 'stable',
     'author' => 'Benni Mack',
     'author_email' => 'benni@typo3.org',
     'author_company' => '',
-    'version' => '3.0.0',
+    'version' => '3.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.9.99',
+            'typo3' => '13.4.0-13.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
